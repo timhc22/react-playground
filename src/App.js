022228d8c92edc0eaeb2ba0fdac769b38ce5942f
Timothy Coleman <timhc22@gmail.com>
2020-03-20 22:55:52 +0000

    Add CRUD text

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+      <div className="container">
+        <h1>CRUD App with Hooks</h1>
+        <div className="flex-row">
+          <div className="flex-large">
+            <h2>Add user</h2>
+          </div>
+          <div className="flex-large">
+            <h2>View users</h2>
+          </div>
+        </div>
+      </div>
   );
 }
 
