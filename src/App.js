132e2ce6951d7338aaf930a5c680bbf5a54eb28d
Timothy Coleman <timhc22@gmail.com>
2020-03-20 23:58:32 +0000

    Add user table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserTable from "./tables/UserTable"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
           </div>
           <div className="flex-large">
             <h2>View users</h2>
+            <UserTable />
           </div>
         </div>
       </div>
