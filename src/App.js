839ca1a64fce2f9544198eaf8a4280c879cf2b7e
Timothy Coleman <timhc22@gmail.com>
2020-03-21 00:02:42 +0000

    Add user data and functions to display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UserTable from "./tables/UserTable"
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ]
+
+  const [users, setUsers] = useState(usersData);
+
   return (
       <div className="container">
         <h1>CRUD App with Hooks</h1>
@@ -11,7 +19,7 @@ const App = () => {
           </div>
           <div className="flex-large">
             <h2>View users</h2>
-            <UserTable />
+            <UserTable users={users} />
           </div>
         </div>
       </div>
