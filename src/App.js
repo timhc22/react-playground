67d5d3c64db389155dc2875144815f3d36f2d1ed
Timothy Coleman <timhc22@gmail.com>
2020-03-21 02:21:20 +0000

    Add edit form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import UserTable from "./tables/UserTable"
 import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
 
 const App = () => {
   const usersData = [
@@ -10,6 +11,9 @@ const App = () => {
   ]
 
   const [users, setUsers] = useState(usersData);
+  const [editing, setEditing] = useState(false);
+  const initialFormState = { id: null, name: '', username: '' }
+  const [currentUser, setCurrentUser] = useState(initialFormState)
 
   const addUser = user => {
     user.id = users.length + 1
@@ -20,17 +24,42 @@ const App = () => {
     setUsers(users.filter(user => user.id !== id))
   }
 
+  const editRow = user => {
+    setEditing(true)
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  }
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false)
+    setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+  }
+
   return (
       <div className="container">
         <h1>CRUD App with Hooks</h1>
         <div className="flex-row">
+
           <div className="flex-large">
-            <h2>Add user</h2>
-            <AddUserForm addUser={addUser} />
+            {editing ? (
+              <div>
+                <h2>Edit user</h2>
+                <EditUserForm
+                  editing={editing}
+                  setEditing={setEditing}
+                  currentUser={currentUser}
+                  updateUser={updateUser} />
+              </div>
+            ) : (
+              <div>
+                <h2>Add user</h2>
+                <AddUserForm addUser={addUser} />
+              </div>
+            )}
           </div>
+
           <div className="flex-large">
             <h2>View users</h2>
-            <UserTable users={users} deleteUser={deleteUser}/>
+            <UserTable users={users} deleteUser={deleteUser} editRow={editRow}/>
           </div>
         </div>
       </div>
