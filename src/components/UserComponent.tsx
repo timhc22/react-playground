3e8014555772b71abf452cb0c6b83bb3a9be9231
Timothy Coleman <timhc22@gmail.com>
2020-03-22 02:22:14 +0000

    Beginning to sort and merge Users handling

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { User } from "../interfaces/User";
 
-export const UserComponent = (user: User): JSX.Element => (
+// TODO FIX THIS TO BE USER
+export const UserComponent = (user: any): JSX.Element => (
     <article className="user-excerpt">
         <h2>{user.name}</h2>
     </article>
--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,72 +1,101 @@
-import React, { useState } from 'react';
-import UserTable from "../tables/UserTable";
-import AddUserForm from '../forms/AddUserForm';
-import EditUserForm from '../forms/EditUserForm';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux'; // enables redux state to be in props of component
+// import UserTable from "../tables/UserTable";
+// import AddUserForm from '../forms/AddUserForm';
+// import EditUserForm from '../forms/EditUserForm';
+import { fetchUsers } from '../actions/usersActions';
+import { UserComponent } from "../components/UserComponent";
 import { User } from "../interfaces/User";
 
-const usersData: User[] = [
-    { id: 1, name: 'Tania', username: 'floppydiskette' },
-    { id: 2, name: 'Craig', username: 'siliconeidolon' },
-    { id: 3, name: 'Ben', username: 'benisphere' },
-];
+// const usersData: User[] = [
+//     { id: 1, name: 'Tania', username: 'floppydiskette' },
+//     { id: 2, name: 'Craig', username: 'siliconeidolon' },
+//     { id: 3, name: 'Ben', username: 'benisphere' },
+// ];
+//
+// const initialFormState: User = { id: null, name: '', username: '' };
 
-const initialFormState: User = { id: null, name: '', username: '' };
+const UsersPage = ({dispatch, users, loading, hasErrors}: any): JSX.Element => {
+    // todo check hooks integration with this (especially on users)
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [dispatch]);
 
-const UsersPage = (): JSX.Element => {
-    const [users, setUsers] = useState<User[]>(usersData);
-    const [editing, setEditing] = useState<boolean>(false);
-    const [currentUser, setCurrentUser] = useState<User>(initialFormState);
-
-    const addUser = (user: User): void => {
-        user.id = users.length + 1;
-        setUsers([...users, user]);
-    };
-
-    const deleteUser = (id: number): void => {
-        setEditing(false);
-        setUsers(users.filter(user => user.id !== id));
-    };
-
-    const editRow = (user: User): void => {
-        setEditing(true);
-        setCurrentUser({ id: user.id, name: user.name, username: user.username });
-    };
-
-    const updateUser = (id: number, updatedUser: User): void => {
-        setEditing(false);
-        setUsers(users.map(user => (user.id === id ? updatedUser : user)));
+    const renderUsers = () => {
+        if (loading) return <p>Loading Users...</p>;
+        if (hasErrors) return <p>Unable to display users.</p>;
+        return users.map((user: User) => <UserComponent key={user.id} />)
     };
 
     return (
-        <div className="container">
-            <h1>CRUD App with Hooks</h1>
-            <div className="flex-row">
-
-                <div className="flex-large">
-                    {editing ? (
-                        <div>
-                            <h2>Edit user</h2>
-                            <EditUserForm
-                                editing={editing}
-                                setEditing={setEditing}
-                                currentUser={currentUser}
-                                updateUser={updateUser} />
-                        </div>
-                    ) : (
-                        <div>
-                            <h2>Add user</h2>
-                            <AddUserForm addUser={addUser} />
-                        </div>
-                    )}
-                </div>
-
-                <div className="flex-large">
-                    <h2>View users</h2>
-                    <UserTable users={users} deleteUser={deleteUser} editRow={editRow}/>
-                </div>
-            </div>
-        </div>
+        <section>
+            <h1>Users</h1>
+            {renderUsers()}
+        </section>
     )
+
+    // const [users, setUsers] = useState<User[]>(usersData);
+    // const [editing, setEditing] = useState<boolean>(false);
+    // const [currentUser, setCurrentUser] = useState<User>(initialFormState);
+    //
+    // const addUser = (user: User): void => {
+    //     user.id = users.length + 1;
+    //     setUsers([...users, user]);
+    // };
+    //
+    // const deleteUser = (id: number): void => {
+    //     setEditing(false);
+    //     setUsers(users.filter(user => user.id !== id));
+    // };
+    //
+    // const editRow = (user: User): void => {
+    //     setEditing(true);
+    //     setCurrentUser({ id: user.id, name: user.name, username: user.username });
+    // };
+    //
+    // const updateUser = (id: number, updatedUser: User): void => {
+    //     setEditing(false);
+    //     setUsers(users.map(user => (user.id === id ? updatedUser : user)));
+    // };
+    //
+    // return (
+    //     <div className="container">
+    //         <h1>CRUD App with Hooks</h1>
+    //         <div className="flex-row">
+    //
+    //             <div className="flex-large">
+    //                 {editing ? (
+    //                     <div>
+    //                         <h2>Edit user</h2>
+    //                         <EditUserForm
+    //                             editing={editing}
+    //                             setEditing={setEditing}
+    //                             currentUser={currentUser}
+    //                             updateUser={updateUser} />
+    //                     </div>
+    //                 ) : (
+    //                     <div>
+    //                         <h2>Add user</h2>
+    //                         <AddUserForm addUser={addUser} />
+    //                     </div>
+    //                 )}
+    //             </div>
+    //
+    //             <div className="flex-large">
+    //                 <h2>View users</h2>
+    //                 <UserTable users={users} deleteUser={deleteUser} editRow={editRow}/>
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
 };
 
-export default UsersPage;
+// map redux state to react component props
+const mapStateToProps = (state: any) => ({
+    loading: state.users.loading,
+    users: state.users.users,
+    hasErrors: state.users.hasErrors,
+});
+
+// connect redux to react
+export default connect(mapStateToProps)(UsersPage);
